c8a5dcfa5b9d58c3c515d888271e4e6169cdc241
Thewolf8 <140300286+Thewolf8@users.noreply.github.com>
2026-07-03 00:46:05 +0100

    Add files via upload

--- android/app/src/main/java/com/homemed/cabinet/MainActivity.java
+++ android/app/src/main/java/com/homemed/cabinet/MainActivity.java
@@ -1,13 +1,5 @@
 package com.homemed.cabinet;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        // Register local Capacitor plugins before the bridge initialises
-        registerPlugin(TextScanPlugin.class);
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
