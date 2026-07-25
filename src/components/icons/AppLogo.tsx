b0985e38633f437d68445947e2e5194dcadde0c9
Thewolf8 <140300286+Thewolf8@users.noreply.github.com>
2026-07-25 20:20:07 +0100

    Update AppLogo.tsx

--- src/components/icons/AppLogo.tsx
+++ src/components/icons/AppLogo.tsx
@@ -8,18 +8,25 @@ export default function AppLogo(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g
-        transform="translate(256 256) rotate(-45) translate(-256 -256)"
+      {/* Rounded rectangle */}
+      <rect
+        x="136"
+        y="96"
+        width="240"
+        height="320"
+        rx="42"
         stroke="currentColor"
         strokeWidth="22"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        {/* Capsule */}
-        <path d="M136 166H376A90 90 0 0 1 376 346H136A90 90 0 0 1 136 166Z" />
+      />
 
-        {/* Divider */}
-        <path d="M256 166V346" />
+      {/* Plus */}
+      <g
+        stroke="#1E6BFF"
+        strokeWidth="28"
+        strokeLinecap="round"
+      >
+        <path d="M256 180V332" />
+        <path d="M180 256H332" />
       </g>
     </svg>
   );
